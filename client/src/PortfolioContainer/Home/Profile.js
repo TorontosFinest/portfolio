ab0789027344db70da7264ba3ede9c7068fa9810
Abdirahman <abdialicanada20@gmail.com>
2022-02-27 18:46:01 -0500

    add socials

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://www.linkedin.com/in/abdi-ali2022/" target="_blank">
+              <i className="fa fa-linkedin"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
