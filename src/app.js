f9a591931440b00beb672aa1b51e567d89987c7a
Abdirahman <abdialicanada20@gmail.com>
2022-03-03 11:57:04 -0500

    add email functionality

--- src/app.js
+++ src/app.js
@@ -7,3 +7,39 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener("click", toggleButton);
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var formStatus = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        formStatus.innerHTML = "Thanks for your submission!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            formStatus.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            formStatus.innerHTML =
+              "Oops! There was a problem submitting your form";
+          }
+        });
+      }
+    })
+    .catch(() => {
+      formStatus.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+form.addEventListener("submit", handleSubmit);
